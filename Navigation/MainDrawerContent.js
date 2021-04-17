1b577325e26263043d33d0920acd7a3fbe9b2833
Dominique Kanyik <domykanyiktesh01@gmail.com>
2021-04-17 15:14:09 +0200

    Driver App 2.0.156
    [*] Major layout bug fix.

--- Navigation/MainDrawerContent.js
+++ Navigation/MainDrawerContent.js
@@ -88,7 +88,7 @@ export function MainDrawerContent(props) {
               color: '#AFAFAF',
               textAlign: 'right',
             }}>
-            v2.0.155
+            v2.0.156
           </Text>
         </View>
       </SafeAreaView>
